e1a644ea2ecff5e4acf6cef91d76d605b2dba99e
Alexi Maschas <alexi.maschas@gmail.com>
2014-01-05 04:04:45 -0500

    simplifying engine module

--- module/Engine/EngineController.cs
+++ module/Engine/EngineController.cs
@@ -6,15 +6,15 @@ public class EngineController : MonoBehaviour, ModuleInterface {
 
 	private Engine engine;
 	private EngineView engineView;
-	public Dictionary <string, ModuleInterface> shipModules;
+	// public Dictionary <string, ModuleInterface> shipModules;
 
 	void Start () {
-		shipModules = transform.parent.gameObject.GetComponent<ShipController>().modules;
+		// shipModules = transform.parent.gameObject.GetComponent<ShipController>().modules;
 		engineView = gameObject.GetComponent<EngineView>();
 		engine = gameObject.GetComponent<Engine>();
 		engineView.enabled = false;
-		engine.thrustDuration = 0.0f;
-		engine.isRunning = false;
+		// engine.thrustDuration = 0.0f;
+		// engine.isRunning = false;
 	}
 
 	public string Name () {
@@ -30,34 +30,34 @@ public class EngineController : MonoBehaviour, ModuleInterface {
 		engineView.enabled = false;
 	}
 
-	public bool HasAction () {
-		if(engine.thrustDuration > 0) {
-			return true;
-		}
-		else return false;
-	}
+	// public bool HasAction () {
+	// 	if(engine.thrustDuration > 0) {
+	// 		return true;
+	// 	}
+	// 	else return false;
+	// }
 	
-	public ModuleAction GetAction () {
-		ModuleAction action = new ModuleAction(this, Vector3.zero, engine.thrustDuration);
-		engine.thrustDuration = 0;
-		return action;
-	}
+	// public ModuleAction GetAction () {
+	// 	ModuleAction action = new ModuleAction(this, Vector3.zero, engine.thrustDuration);
+	// 	engine.thrustDuration = 0;
+	// 	return action;
+	// }
 	
 	public void Run (ModuleAction action) {
-		if(!engine.isRunning) {
-			engine.isRunning = true;
-			engine.thrustDuration = action.duration;
-		}
-		if(engine.thrustDuration <= 0) {
-			engine.isRunning = false;
-		}
-		transform.parent.gameObject.GetComponent<ShipController>().AddThrust(engine.thrust);
+		// if(!engine.isRunning) {
+		// 	engine.isRunning = true;
+		// 	engine.thrustDuration = action.duration;
+		// }
+		// if(engine.thrustDuration <= 0) {
+		// 	engine.isRunning = false;
+		// }
+		transform.parent.gameObject.GetComponent<ShipController>().AddThrust(engine.thrust * time.deltaTime);
 		// Debug.Log(engine.thrustDuration);
 		// Debug.Log(Time.deltaTime);
-		engine.thrustDuration -= Time.deltaTime;
+		// engine.thrustDuration -= Time.deltaTime;
 	}
 	
-	public bool IsRunning () {
-		return engine.isRunning;
-	}
+	// public bool IsRunning () {
+	// 	return engine.isRunning;
+	// }
 }
