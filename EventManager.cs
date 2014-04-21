5e7b07b93cb39fb1876619ae0ba41647d0fdc7b7
Alexi Maschas <alexi.maschas@gmail.com>
2014-04-21 02:09:43 -0400

    sketching out eventmanager

--- EventManager.cs
+++ EventManager.cs
@@ -6,6 +6,24 @@ using System.Collections;
 // Utility class for raising events
 public class EventManager {
 
+	// Dict of all events by broadcaster (this might only work with an event manager per-ship, else the dict gets huge)
+	protected Dictionary<string, Dictionary<<string, EventHandler>> EventBroadcasters;
+
+	// Register all module events with EventManager
+	public void RegisterEvents( ModuleControllerInterface moduleController ){
+
+	}
+
+	// Register an event with EventManager
+	protected void RegisterEvent( string eventBroadcaster, EventHandler event){
+
+	}
+
+	// Listen to an object and an event
+	public void SetupListener( ModuleControllerInterface listener, string broadcaster, string event ){
+
+	}
+
 	// will EventArgs work here polymorphically?
 	public static void RaiseEvent ( EventHandler eventHandler, EventArgs args = EventArgs.Empty ) {
 
